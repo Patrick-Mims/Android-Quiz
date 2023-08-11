34ba14e37ce64a1ae33ad5bbda582a474fc16a23
weatherpub <patrick@bayareamedia.net>
2023-08-10 20:12:00 -0700

    Implemented nextQuestion() method

--- app/src/main/java/edu/sfsu/quiz/MainActivity.java
+++ app/src/main/java/edu/sfsu/quiz/MainActivity.java
@@ -6,14 +6,33 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import edu.sfsu.quiz.models.Question;
+
 public class MainActivity extends AppCompatActivity {
+    private int index = 0;
+    private Button nextButton;
+    private TextView questionTextView;
+    private Question[] questions = new Question[] {
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_asia, true),
+            new Question(R.string.question_australia, false),
+            new Question(R.string.question_china, true),
+            new Question(R.string.question_europe, false),
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get a reference to the question_text_view
+        int question = questions[index].getId();
+
+        questionTextView = (TextView) findViewById(R.id.question_text_view);
+        questionTextView.setText(question);
 
         Button true_button = (Button) findViewById(R.id.true_btn);
         Button false_button = (Button) findViewById(R.id.false_btn);
@@ -35,5 +54,20 @@ public class MainActivity extends AppCompatActivity {
                 toast.show();
             }
         });
+
+        nextButton = (Button) findViewById(R.id.next_button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                index = (index + 1) % questions.length;
+                nextQuestion();
+            }
+        });
+        nextQuestion();
+    }
+
+    public void nextQuestion() {
+        int question = questions[index].getId();
+        questionTextView.setText(question);
     }
 }
