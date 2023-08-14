b2f44841ebf844ad3961b76f7eed16f53d3c8507
weatherpub <patrick@bayareamedia.net>
2023-08-14 09:55:49 -0700

    MainActivity is alive an working well.

--- app/src/main/java/edu/sfsu/quiz/MainActivity.java
+++ app/src/main/java/edu/sfsu/quiz/MainActivity.java
@@ -3,15 +3,12 @@ package edu.sfsu.quiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.android.material.internal.TextDrawableHelper;
 
 import java.util.ArrayList;
 import java.util.List;
